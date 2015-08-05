2faa4d93dfc78a87d69c2bf658168f6968283177
Jesse Schalken <me@jesseschalken.com>
2015-08-05 16:11:30 +1000

    CORE-394 Use 'exec' when running a command with only arguments, to avoid shell overhead

--- System.php
+++ System.php
@@ -245,7 +245,9 @@ abstract class System implements FileSystem
 	 */
 	final function runCommandArgs( array $command, $stdIn = '' )
 	{
-		return $this->runCommand( $this->escapeCmdArgs( $command ), $stdIn );
+		// Use 'exec' so the command is a direct child of this process, without an 'sh -c ...'
+		// process in between.
+		return $this->runCommand( 'exec ' . $this->escapeCmdArgs( $command ), $stdIn );
 	}
 
 	/**
