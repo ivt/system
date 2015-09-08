4fb76ff22fd7d084f63950971db381781432349c
Gregory Tod <greg.tod@ivt.com.au>
2015-09-08 16:52:29 +1000

    Add some trivial unit tests

---
+++ src/Test.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace IVT\System\Test;
+
+use IVT\System\LocalSystem;
+
+class TestLocal extends \PHPUnit_Framework_TestCase
+{
+    function testProcess()
+    {
+        $local = LocalSystem::create();
+
+        $result = $local->runCommand("cat /etc/passwd | wc -l");
+        $this->assertGreaterThan( 0, $result->stdOut() );
+
+        $result = $local->runCommand("true");
+        $this->assertTrue( $result->succeeded() );
+
+        $result = $local->runCommand("false");
+        $this->assertTrue( $result->failed() );
+    }
+}
